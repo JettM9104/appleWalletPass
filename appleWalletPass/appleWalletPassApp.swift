9a0bd5139c527fe77d8a8e66a8fd35a22604d9ae
JMu <64600882+JettM9104@users.noreply.github.com>
2025-05-08 10:36:35 -0400

    Initial Commit

---
+++ appleWalletPass/appleWalletPassApp.swift
@@ -0,0 +1,17 @@
+//
+//  appleWalletPassApp.swift
+//  appleWalletPass
+//
+//  Created by Jett Mu on 2025-05-08.
+//
+
+import SwiftUI
+
+@main
+struct appleWalletPassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ appleWalletPassTests/appleWalletPassTests.swift
@@ -0,0 +1,17 @@
+//
+//  appleWalletPassTests.swift
+//  appleWalletPassTests
+//
+//  Created by Jett Mu on 2025-05-08.
+//
+
+import Testing
+@testable import appleWalletPass
+
+struct appleWalletPassTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
